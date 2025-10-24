bbabac9bf234379fd000969ce15167a75fb16122
stephen_adah <mailstephenreal@gmail.com>
2025-10-24 12:24:10 +0100

    DEV: Created the users interfaces

---
+++ src/interfaces/user.ts
@@ -0,0 +1,45 @@
+import { Document } from 'mongoose';
+
+export interface IUser extends Document {
+  userId: string;
+  email: string;
+  passwordHash: string;
+  userType: 'client' | 'provider';
+  isVerified: boolean;
+  verificationCode?: string;
+  verificationCodeExpires?: Date;
+  isPremium: boolean; // For revenue features (subscriptions)
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface LocationDetails {
+  address: string;
+  city: string;
+  country: string;
+  state: string;
+  postalCode?: string;
+  coordinates?: [number, number]; // Longitude, latitude for geospatial matching (Optional)
+}
+
+export interface IClient extends IUser {
+  firstName: string;
+  lastName: string;
+  phone?: string;
+  location: LocationDetails;
+}
+
+export interface IProvider extends IUser {
+  firstName: string;
+  lastName: string;
+  phone?: string;
+  location?: LocationDetails;
+  service: {
+    serviceCategory: string;
+    registrationStatus: string;
+    businessName: string;
+    website?: string; // Optional per UI
+    serviceDescription: string;
+  };
+  availability: boolean; // For matching available providers
+}
