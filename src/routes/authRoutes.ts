ca57a4e6c83376c6ab817214ca93dcb39fdb41b3
stephen_adah <mailstephenreal@gmail.com>
2025-10-29 23:13:18 +0100

    MOD: Added login

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { register, verifyEmail } from '../controllers/authController';
+import { register, login, verifyEmail } from '../controllers/authController';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
 router.post('/verify-email', verifyEmail);
 
 export default router;
