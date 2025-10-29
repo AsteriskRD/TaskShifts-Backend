724fb9db34bd7ac31d6b29ee0a5913bbe99f2b32
stephen_adah <mailstephenreal@gmail.com>
2025-10-29 17:27:34 +0100

    DEV: Created interface for postalcache

---
+++ src/interfaces/postalCache.ts
@@ -0,0 +1,11 @@
+import mongoose, { Document } from 'mongoose';
+
+export interface IPostalCache extends Document {
+  postalCode: string;
+  country: string;
+  latitude: number;
+  longitude: number;
+  provider: 'opencage' | 'google';
+  createdAt?: Date;
+  updatedAt?: Date;
+}
