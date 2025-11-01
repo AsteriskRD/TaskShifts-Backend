e4a63c201f97d18ce864c2b3b3804fba5478ade9
stephen_adah <mailstephenreal@gmail.com>
2025-11-01 08:04:03 +0100

    MOD: correct createTransporter() to createTransport()

--- src/services/verifyEmailService.ts
+++ src/services/verifyEmailService.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 export const sendVerificationEmail = async (email: string, verificationCode: string) => {
   try {
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST || 'smtp.gmail.com',
       port: parseInt(process.env.SMTP_PORT || '587'),
       secure: false,
