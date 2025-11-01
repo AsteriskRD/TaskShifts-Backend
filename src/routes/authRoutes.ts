b56a061ecb486d3a3ee57cd432d409320df8a971
stephen_adah <mailstephenreal@gmail.com>
2025-11-01 16:43:00 +0100

    MOD: Added logout routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { register, login, verifyEmail } from '../controllers/authController';
+import { register, login, verifyEmail, logout } from '../controllers/authController';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/verify-email', verifyEmail);
+router.post('/logout', verifyToken, logout);
 
 export default router;
