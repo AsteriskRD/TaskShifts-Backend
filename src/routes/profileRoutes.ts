693b526267a63ff3fed3f47ea61e08d3541e496e
stephen_adah <mailstephenreal@gmail.com>
2025-11-03 09:34:12 +0100

    MOD: Modified userType check and assigning of model

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { completeClientProfile } from '../controllers/profileController';
+import { completeProfile } from '../controllers/profileController';
 import { verifyToken } from '../middleware/auth';
 
 const router = express.Router();
 
 // Only logged-in users (with JWT) can complete their profile
-router.post('/complete-profile', verifyToken, completeClientProfile);
+router.post('/complete-profile', verifyToken, completeProfile);
 
 export default router;
