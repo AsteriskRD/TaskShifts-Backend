62ca629f6fb77e61b79cb6bce973dd9881310d15
stephen_adah <mailstephenreal@gmail.com>
2025-11-30 12:03:16 +0100

    MOD: Added get and patch routes

--- src/routes/kycStep1.routes.ts
+++ src/routes/kycStep1.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { kycStep1 } from '../controllers/kycStep1.controller';
+import { kycStep1, getKycStep1Data, updateKycStep1Data } from '../controllers/kycStep1.controller';
 import multer from 'multer';
 import { verifyToken } from '../middleware/auth';
 
@@ -12,8 +12,19 @@ const router = Router();
 router.post(
   '/step1',
   verifyToken,
-  upload.single('profilePicture'), // field name in frontend form
+  upload.single('profilePicture'), // accepts file named "profilePicture"
   kycStep1
+);
+router.get(
+  '/step1/data',
+  verifyToken,
+  getKycStep1Data
+);
+router.patch(
+  '/step1/update', 
+  verifyToken, 
+  upload.single('profilePicture'),  // accepts file named "profilePicture"
+  updateKycStep1Data
 );
 
 export default router;
