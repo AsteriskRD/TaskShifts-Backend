74a86c1f69523f5e95fc98e156b77793b942b591
stephen_adah <mailstephenreal@gmail.com>
2025-12-17 00:17:45 +0100

    MOD: Modified reset link

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -106,7 +106,7 @@ export const register = async (req: Request, res: Response) => {
     };
 
     // Construct verification link
-    const verificationLink = `${process.env.FRONTEND_URL || 'http://localhost:3000'}/success-verify?email=${encodeURIComponent(email)}&code=${verificationCode}`;
+    const verificationLink = `${process.env.FRONTEND_URL || 'http://localhost:3000'}/link-verify-email?email=${encodeURIComponent(email)}&code=${verificationCode}`;
 
     if (userType === 'client') {
       const user = await (ClientModel as typeof ClientModel).create(userData);
